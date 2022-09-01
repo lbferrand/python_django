4924fbfec93000532ebc35183ecfa38fb1102b8f
lbferrand <lbferrand@gmail.com>
2022-08-31 20:23:29 -0400

    Turn off debugging

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7qk2-0r@&b@bjy$3ip&a*19tl)dn#%ib#0*xd_9jtkpa2er=fn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
