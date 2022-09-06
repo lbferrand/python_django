d4ecab62a68ec017fdd3e58632419798876e7023
lbferrand <lbferrand@gmail.com>
2022-09-06 09:10:05 -0400

    Create Category model

--- mysite/myblog/forms.py
+++ mysite/myblog/forms.py
@@ -1,16 +1,31 @@
 from django import forms
-from .models import Post
+from .models import Post, Category
+
+#hardcode choices inside category - Create a list with categories
+#choices = [('Coding', 'Coding'), ('Movies', 'Movies'), ('Books', 'Books')]
+
+#inside category should be "forms.Select(choices=choices" 
+
+#when we don't hardcode the categories, we create a query to grab each category
+choices = Category.objects.all().values_list('name', 'name')
+
+choice_list = []
+
+for item in choices:
+  choice_list.append(item)
+
 
 
 class PostForm(forms.ModelForm):
   class Meta:
     model = Post
-    fields = ('title', 'title_tag', 'author', 'body')
+    fields = ('title', 'title_tag', 'author', 'category', 'body')
 
     widgets = {
-      'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Please add your title'}),
+      'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Add title here'}),
       'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-      'author': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Please select author'}),
+      'author': forms.Select(attrs={'class': 'form-control'}),
+      'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
       'body': forms.Textarea(attrs={'class': 'form-control'}),
     }
 
--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Post, Category
 from .forms import PostForm, EditForm
 from django.urls import reverse_lazy
 
@@ -14,25 +14,61 @@ class HomeView(ListView):
   ordering = ['-post_date']
   #ordering = ['-id']
 
+  def get_context_data(self, *arg, **kwargs):
+    cat_menu = Category.objects.all()
+    context = super(HomeView, self).get_context_data(*arg, **kwargs)
+    context["cat_menu"] = cat_menu
+    return context
+
 
 class ArticleDetailView(DetailView):
   model = Post
   template_name = "article_details.html"
 
+  def get_context_data(self, *arg, **kwargs):
+    cat_menu = Category.objects.all()
+    context = super(ArticleDetailView, self).get_context_data(*arg, **kwargs)
+    context["cat_menu"] = cat_menu
+    return context
+
 class AddPostView(CreateView):
   model = Post
   form_class = PostForm
   template_name = "add_post.html"
-  #fields = '__all__'
-  # fields = ('title', 'author', 'body')
+
+class AddCategoryView(CreateView):
+  model = Category
+  template_name = "add_category.html"
+  fields = '__all__'
+
+def CategoryListView(request):
+  cat_menu_list = Category.objects.all()
+  return render(request, 'category_list.html', {'cat_menu_list':cat_menu_list})
+
+def CategoryView(request, cats):
+  category_posts = Post.objects.filter(category=cats)
+  return render(request, 'categories.html', {'cats':cats, 'category_posts':category_posts})
+
 
 class UpdatePostView(UpdateView):
   model = Post
   form_class = EditForm
   template_name = 'update_post.html'
-  #fields = ['title', 'title_tag', 'body']
+  #fields = '__all__'
+
+  def get_context_data(self, *arg, **kwargs):
+      cat_menu = Category.objects.all()
+      context = super(UpdatePostView, self).get_context_data(*arg, **kwargs)
+      context["cat_menu"] = cat_menu
+      return context
 
 class DeletePostView(DeleteView):
   model = Post
   template_name = 'delete_post.html'
   success_url = reverse_lazy('home')
+
+  def get_context_data(self, *arg, **kwargs):
+      cat_menu = Category.objects.all()
+      context = super(DeletePostView, self).get_context_data(*arg, **kwargs)
+      context["cat_menu"] = cat_menu
+      return context
